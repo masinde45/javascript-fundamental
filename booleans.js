aba28fc6ed8c4502b4e9181f30133d14dfdfdf86
masinde45 <linetmasinde45@gmail.com>
2022-10-18 07:50:03 -0700

    firstcommit

---
+++ booleans.js
@@ -0,0 +1,11 @@
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+let gameOver = false;
+console.log(gameOver);
+
+const isWaterWet = true;
+console.log(isWaterWet);
+
+let animal = "dog";
+console.log(animal.length);
---
+++ strings.js
@@ -0,0 +1,32 @@
+let animal = "dog";
+console.log(animal.length);
+
+//access individual character in a string
+console.log(animal[3]);
+let msg = 'i am king';
+let yellMsg = msg.toUpperCase();
+console.log(yellMsg);
+let angry = "LeAVE Me aLoNe";
+let lower = angry.toLowerCase();
+console.log(lower);
+let greetings = ' leave me alone plz ';
+console.log(greetings);
+  let newGreetings = greetings.trim();
+  console.log(newGreetings);
+  let tvShow = 'catdog';
+  let firstIndex = tvShow.indexOf('cat');
+  console.log(firstIndex);
+  //slice
+  let str = 'lokichogio';
+  console.log(str);
+  let partOfStr = str.slice(2,8);
+  console.log(partOfStr);
+  //replaceAll
+  let annoyingLaugh = 'teehee so funny! teehee!';
+  console.log(annoyingLaugh);
+  let replacer = annoyingLaugh.replaceAll('teehee', 'haha');
+  console.log(replacer);
+  //string escape
+  let myName = 'linet\nmasinde';
+  console.log(myName);
+  console.log('hello\'world');
